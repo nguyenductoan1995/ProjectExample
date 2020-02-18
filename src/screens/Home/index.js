2b4d04ea0e0dffb6a82f71e2bb1c7c766dbf2779
nguyenductoan1995 <nguyenductoann@gmail.com>
2020-02-18 23:09:58 +0700

    install lib bluetooth

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Button } from 'react-native'
+// I changed this to export default App
+class BluetoothScanner extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
 
-export default class componentName extends React.PureComponent {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: 'red' }} />
+      <View style={{ padding: 50 }}>
+        <Text>Bluetooth scanner</Text>
+        <Button onPress={() => {}} title="Start scanning" />
+        <Button onPress={() => {}} title="Stop scanning" />
+      </View>
     )
   }
 }
+export default BluetoothScanner
