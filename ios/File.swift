b63f39fc1b62e4ba2cc949a16bcfec76fef6a8a9
nguyenductoan1995 <nguyenductoann@gmail.com>
2020-02-22 23:17:05 +0700

    update Scan bluetooth
    ios

---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ProjectExample
+//
+//  Created by Nguyen Duc Toan on 2/22/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,26 +3,81 @@ import {
   View,
   Text,
   Button } from 'react-native'
+import { BleManager } from 'react-native-ble-plx'
+import { FlatList } from 'react-native-gesture-handler'
+import { get } from 'lodash'
 // I changed this to export default App
 class BluetoothScanner extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
+    this.manager = new BleManager()
     this.state = {
+      list: [],
     }
   }
 
   componentDidMount() {
-
+    const subscription = this.manager.onStateChange((state) => {
+      // console.tron.log('state', state)
+      if (state === 'PoweredOn') {
+        this.scanAndConnect()
+        subscription.remove()
+      }
+    }, true)
   }
 
-  render() {
-    return (
-      <View style={{ padding: 50 }}>
-        <Text>Bluetooth scanner</Text>
-        <Button onPress={() => {}} title="Start scanning" />
-        <Button onPress={() => {}} title="Stop scanning" />
-      </View>
-    )
+  onScan = () => {
+    this.setState({ list: [] })
+    const subscription = this.manager.onStateChange((state) => {
+      // console.tron.log('state', state)
+      if (state === 'PoweredOn') {
+        this.scanAndConnect()
+        subscription.remove()
+      }
+    }, true)
   }
+
+  scanAndConnect() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        return
+      }
+
+      // Check if it is a device you are looking for based on advertisement data
+      // or other criteria.
+      console.tron.log('device name', device)
+      const { list } = this.state
+      const dif = list ? list.find((item) => get(item, 'id') === get(device, 'id')) : false
+      if (!dif) {
+        const data = list
+        data.push(device)
+        this.setState({ list: data })
+      }
+    })
+  }
+
+renderItem = ({ item }) => (
+  <View style={{ borderBottomWidth: 1, borderBottomColor: 'red' }}>
+    <Text>{item.name}</Text>
+    <Text>{item.id}</Text>
+  </View>
+)
+
+render() {
+  const { list } = this.state
+  return (
+    <View style={{ padding: 50 }}>
+      <Text>Bluetooth scanner</Text>
+      <Button onPress={this.onScan} title="Start scanning" />
+      <Button onPress={() => {}} title="Stop scanning" />
+      <FlatList
+        data={list}
+        keyExtractor={(item, index) => `${index}`}
+        renderItem={this.renderItem}
+      />
+    </View>
+  )
+}
 }
 export default BluetoothScanner
